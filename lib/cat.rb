52391abb38b6816674f49177951101d4fac30cb5
Roberto Medina <robby.medina562@gmail.com>
2019-09-27 06:25:07 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,6 +2,12 @@ class Cat
   @@all = []
   attr_accessor :owner, :mood
   attr_reader :name
-  def initialize 
-    
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    @all << self
+  end
+  def self.all
+    @@all
+  end
 end
