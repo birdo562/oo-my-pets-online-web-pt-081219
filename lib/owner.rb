896af3ff04aa17eeffb6f41af084b689c86342aa
Roberto Medina <robby.medina562@gmail.com>
2019-09-27 07:15:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,10 +29,10 @@ end
     Cat.all.select {|cat| cat.owner == self}
   end
   def buy_cat(name)
-    cat.new(name, self)
+    Cat.new(name, self)
   end
     def buy_dog(name)
-      dog.new(name, self)
+      Dog.new(name, self)
     end
 end
 
