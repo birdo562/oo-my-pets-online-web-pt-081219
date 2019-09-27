3f79d11979d162d54a72d2d88c73498345398003
Roberto Medina <robby.medina562@gmail.com>
2019-09-27 06:58:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,9 @@ class Dog
   attr_accessor :owner, :mood
   attr_reader :name
   def initialize(owner, name, mood="nervous")
-    @name = name
-    @owner = owner
-    @mood = mood
+    @name= name
+    @owner= owner
+    @mood= mood
     @@dogs << self
   end
   def self.all
